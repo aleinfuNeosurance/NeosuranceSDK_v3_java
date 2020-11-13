89b5c3f19001a7fcfca8948f3b0c220767bac5b1
ok_neosurance <ok_neosurance@MacBook-Pro.station>
2020-11-13 15:28:31 +0100

    commit nsr sdk (3.0.19)

--- NeosuranceSDK_v3_java/src/main/java/eu/nsrsdk/v3java/NSRDefaultSecurity.java
+++ NeosuranceSDK_v3_java/src/main/java/eu/nsrsdk/v3java/NSRDefaultSecurity.java
@@ -4,14 +4,8 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Build;
 
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.Message;
-
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.Iterator;
 import eu.nsrsdk.utils.NSRUtils;
@@ -19,115 +13,18 @@ import eu.nsrsdk.utils.NSRUtils;
 @RequiresApi(api = Build.VERSION_CODES.KITKAT)
 public class NSRDefaultSecurity implements NSRSecurityDelegate {
 
-	private static AsynchRequestCustom asynchRequestCustom = null;
-	private static HandlerThread nsrNetworkingHandlerThread = null;
-
 	public void secureRequest(final Context ctx, final String endpoint, final JSONObject payload, final JSONObject headers, final NSRSecurityResponse completionHandler) throws Exception {
 		try {
 			String url = NSRUtils.getSettings(ctx).getString("base_url") + endpoint;
 			NSRLog.d("NSRDefaultSecurity: " + url);
-
-			if(nsrNetworkingHandlerThread == null)
-				nsrNetworkingHandlerThread = new HandlerThread("nsrNetworkingHandlerThread");
-
-			if(asynchRequestCustom == null)
-				asynchRequestCustom = new AsynchRequestCustom(nsrNetworkingHandlerThread);
-
-			asynchRequestCustom.doAsyncTask(url, payload, headers, completionHandler);
-
-			//AsynchRequest asynchRequest = new AsynchRequest(url, payload, headers, completionHandler);
-			//asynchRequest.execute();
+			AsynchRequest asynchRequest = new AsynchRequest(url, payload, headers, completionHandler);
+			asynchRequest.execute();
 		} catch (Exception e) {
 			NSRLog.e(e.getMessage());
 			throw e;
 		}
 	}
 
-	private class AsynchRequestCustom{
-
-		public String url;
-		public JSONObject payload;
-		public JSONObject headers;
-		public NSRSecurityResponse completionHandler;
-
-		public HandlerThread nsrNHandlerThread = null;
-
-		public AsynchRequestCustom(HandlerThread nsrNetworkingHandlerThread) {
-			this.nsrNHandlerThread = nsrNetworkingHandlerThread;
-		}
-
-		public void doAsyncTask(final String url, final JSONObject payload, final JSONObject headers, final NSRSecurityResponse completionHandler) {
-
-			this.url = url;
-			this.payload = payload;
-			this.headers = headers;
-			this.completionHandler = completionHandler;
-
-			if(this.nsrNHandlerThread == null)
-				this.nsrNHandlerThread = new HandlerThread("nsrNetworkingHandlerThread");
-
-			Handler asyncHandler = new Handler(this.nsrNHandlerThread.getLooper()){
-				@Override
-				public void handleMessage(@NonNull Message msg) {
-					super.handleMessage(msg);
-					//Object response = msg.obj;
-				}
-			};
-
-			Runnable runnable = new Runnable() {
-				@Override
-				public void run() {
-					// your async code goes here.
-
-					NSRHttpRunner httpRunner = null;
-
-					try {
-
-						httpRunner = new NSRHttpRunner(url);
-
-						if (payload != null)
-							httpRunner.payload(payload.toString(), "application/json");
-
-						if (headers != null) {
-							Iterator<String> keys = headers.keys();
-							while (keys.hasNext()) {
-								String key = keys.next();
-								httpRunner.header(key, headers.getString(key));
-							}
-						}
-						String response = httpRunner.read();
-						NSRLog.d("NSRDefaultSecurity response:" + response);
-
-						completionHandler.completionHandler(new JSONObject(response), null);
-
-					} catch (Exception e) {
-						try {
-							if (httpRunner != null) {
-								NSRLog.e("MSG:" + httpRunner.getMessage());
-								NSRLog.e("Error:" + e.getMessage());
-							}
-							completionHandler.completionHandler(null, e.toString());
-						} catch (Exception ee) {
-							NSRLog.e(ee.toString());
-						}
-					}
-
-				}
-			};
-
-			asyncHandler.post(runnable);
-
-		}
-
-	}
-
-	/*
-	* AsyncTask was intended to enable proper and easy use of the UI thread. However, the most
- 	* common use case was for integrating into UI, and that would cause Context leaks, missed
- 	* callbacks, or crashes on configuration changes. It also has inconsistent behavior on different
- 	* versions of the platform, swallows exceptions from {@code doInBackground}, and does not provide
- 	* much utility over using {@link Executor}s directly
-	*/
 	private class AsynchRequest extends AsyncTask<Void, Void, Object> {
 		private String url;
 		private JSONObject payload;
